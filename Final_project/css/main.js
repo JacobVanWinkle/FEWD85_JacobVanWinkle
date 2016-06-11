eec3a23b08e20b98f94cb874eece65dbac63bc44
JacobVanWinkle <jacob.vanwinkle@gmail.com>
2016-06-11 00:07:19 -0400

    Final Project
    Final Project

--- Final_project/css/main.js
+++ Final_project/css/main.js
@@ -1,8 +1,8 @@
-document.getElementsByTagName("li")[0].addEventListener("click",function(){
-  if(!this.parentNode.getAttribute("class")){
-    this.parentNode.setAttribute("class","slideDown");
-  }
-  else{
-    this.parentNode.removeAttribute("class");
-  }
+
+
+
+
+
+$('button').click(function() {
+  $(this).toggleClass('expanded').siblings('div').slideToggle();
 });
